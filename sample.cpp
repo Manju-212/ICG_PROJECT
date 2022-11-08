dc3575c355d1349e92ddeb288671502d798642b2
Manju-212 <mnrreddy212@gmail.com>
2022-11-08 23:48:03 +0530

    "clouds and truck completed with animation"

--- sample.cpp
+++ sample.cpp
@@ -5,23 +5,81 @@ using namespace std;
 int n = 300;
 int n1 = 300;
 int pl = 0;
-int a = 0;
+int truck_movement = -200;
+int plane_movement = -300;
+int bus_movement = 1500;
+int cloud_movement1 = 0;
+int cloud_movement2 = 0;
+void cloud_circle(float a,float b,int radius);
 void Airplane();
 void Bus();
+void truck();
 void road();
+void Sun();
 void Building();
 void Bird();
+void clouds();
 void Mountain();
 void white_building1();
 void white_building2();
 void airplane_pole();
 void sky_scraper();
+int refsec = 10;
+void Timer(int value) 
+{	
+    glutTimerFunc(refsec, Timer, 10); // next Timer call milliseconds later
+    if(truck_movement<=2500)
+    {
+      truck_movement = truck_movement + 1;
+    }
+    else
+    {
+        truck_movement = -200;
+    }
+     if(bus_movement>=-1500)
+    {
+      bus_movement = bus_movement - 1;
+    }
+    else
+    {
+        bus_movement = 1500;
+    }
+    if(plane_movement<=2500)
+    {
+        plane_movement = plane_movement + 5;
+    }
+    else
+    {
+     
+        plane_movement = -300;
+    }
+    //cloud movement  1
+     if(cloud_movement1>=-2000)
+    {
+        cloud_movement1 = cloud_movement1 - 5;
+    }
+    else
+    {
+        cloud_movement1 = 2200;
+    }
+    //cloud movement 2
+      if(cloud_movement2>=-700)
+    {
+        cloud_movement2 = cloud_movement2 - 5;
+    }
+    else
+    {
+        cloud_movement2 = 2200;
+    }
+	glutPostRedisplay();      // Post re-paint request to activate display()
+}
 void display()
 {
-    glClearColor(0.4, 0.4, 1, 1.0f);
+    glClearColor(0.5, 0.8, 0.9, 1.0f);
+    // glClearColor(1,1,1, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glMatrixMode(GL_PROJECTION);
+    // glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     // gluOrtho2D(-500, 500, -500, 500);
     gluOrtho2D(0, 2000, 0, 2000);
@@ -29,15 +87,233 @@ void display()
     Mountain();
     road();
     sky_scraper();
+    Sun();
     // Bird();
+    clouds();
     Airplane();
     Building();
-
     airplane_pole();
+    truck();
     white_building1();
     white_building2();
     Bus();
+
     glFlush(); // Render now
+}
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 27:
+        exit(0);
+        break;
+    case 's':
+        plane_movement = plane_movement + 100;
+        display();
+        break;
+    case 'd':
+        while(bus_movement>=-2000)
+        {
+        bus_movement = bus_movement - 1;
+        display();
+        }
+        // glutTimerFunc(100,display, 0);
+        break;
+    case 'a':
+        while(truck_movement<=2000)
+        {
+        truck_movement = truck_movement + 1;
+        display();
+        } 
+        break;
+    }
+}
+
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);                 // Initialize GLUT
+    glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+    glutInitWindowSize(320, 320);          // Set the window's initial width & height
+    glutInitWindowPosition(100, 100);      // Position the window's initial top-left corner
+    glutDisplayFunc(display);              // Register display callback handler for window re-paint
+    //glutKeyboardFunc(keyboard);
+    glutTimerFunc(0, Timer, 0);
+    glutMainLoop(); // Enter the event-processing loop
+    return 0;
+}
+void cloud_circle(float a,float b,int radius)
+{
+    float x = radius;
+    float y = 0;
+    glColor3f(1,1,1);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i <= 360; i++)
+    {
+        x = radius * cos(i);
+        y = radius * sin(i);
+        glVertex2f(x + a, y + b);
+    }
+    glEnd();
+}
+void clouds()
+{
+    //cloud 1 
+    cloud_circle(1600 + cloud_movement1,1600,100);
+    cloud_circle(1700 + cloud_movement1,1500,100);
+    cloud_circle(1690 + cloud_movement1,1400,100);
+    cloud_circle(1610 + cloud_movement1,1450,100);
+    cloud_circle(1600 + cloud_movement1,1500,100);
+
+    //cloud 2
+    cloud_circle(600 + cloud_movement2,1700,100);
+    cloud_circle(650 + cloud_movement2,1680,100);
+    cloud_circle(580 + cloud_movement2,1570,100);
+    cloud_circle(490 + cloud_movement2,1650,100);
+    cloud_circle(590 + cloud_movement2,1670,100);
+
+}
+void truck()
+{
+    glPushMatrix();
+    glTranslated(truck_movement,30, 0.0);
+    glColor3ub(37,174,170);
+    // outer layer
+    glBegin(GL_QUADS);
+    glVertex2f(0, 0);
+    glVertex2f(0,80);
+    glVertex2f(300, 80);
+    glVertex2f(300, 0);
+    glEnd();
+
+    glBegin(GL_QUADS);
+    glVertex2f(200,80);
+    glVertex2f(200,160);
+    glVertex2f(250, 160);
+    glVertex2f(300, 80);
+
+
+    glColor3ub(232, 15, 136);
+    glVertex2f(210,100);
+    glVertex2f(250,100);
+    glVertex2f(250, 140);
+    glVertex2f(210, 140);
+    glEnd();
+    glPopMatrix();
+
+
+    glPushMatrix(); // front tyre
+    glTranslated(-50+truck_movement,-40,0.0);
+    glScaled(25.0, 25.0, 0.0);
+    glColor3f(0.78,0.05,0.227);
+    glBegin(GL_POLYGON);
+    glVertex2f(3.0, 2.5);
+    glVertex2f(3.0, 2.6);
+    glVertex2f(3.15, 3.1);
+    glVertex2f(3.2, 3.2);
+    glVertex2f(3.3, 3.35);
+    glVertex2f(3.4, 3.4);
+    glVertex2f(3.5, 3.45);
+    glVertex2f(3.6, 3.55);
+    glVertex2f(3.7, 3.6);
+    glVertex2f(3.8, 3.63);
+    glVertex2f(4.0, 3.65);
+    glVertex2f(4.2, 3.7);
+    glVertex2f(4.4, 3.7);
+    glVertex2f(4.6, 3.65);
+    glVertex2f(4.8, 3.55);
+    glVertex2f(5.0, 3.45);
+    glVertex2f(5.1, 3.4);
+    glVertex2f(5.2, 3.25);
+    glVertex2f(5.3, 3.2);
+    glVertex2f(5.4, 3.0);
+    glVertex2f(5.5, 2.5);
+
+    glVertex2f(5.45, 2.15);
+    glVertex2f(5.4, 1.9);
+    glVertex2f(5.35, 1.8);
+    glVertex2f(5.2, 1.6);
+    glVertex2f(5.0, 1.5);
+    glVertex2f(4.9, 1.4);
+    glVertex2f(4.7, 1.3);
+    glVertex2f(4.6, 1.27);
+    glVertex2f(4.4, 1.25);
+    glVertex2f(4.0, 1.25);
+    glVertex2f(3.9, 1.3);
+    glVertex2f(3.75, 1.35);
+    glVertex2f(3.6, 1.4);
+    glVertex2f(3.45, 1.55);
+    glVertex2f(3.3, 1.7);
+    glVertex2f(3.2, 1.8);
+    glVertex2f(3.1, 2.2);
+    glEnd();
+    glPopMatrix();
+
+
+
+    glPushMatrix(); // front tyre
+    glTranslated(140+truck_movement,-40,0.0);
+    glScaled(25.0, 25.0, 0.0);
+    glColor3f(0.78,0.05,0.227);
+    glBegin(GL_POLYGON);
+    glVertex2f(3.0, 2.5);
+    glVertex2f(3.0, 2.6);
+    glVertex2f(3.15, 3.1);
+    glVertex2f(3.2, 3.2);
+    glVertex2f(3.3, 3.35);
+    glVertex2f(3.4, 3.4);
+    glVertex2f(3.5, 3.45);
+    glVertex2f(3.6, 3.55);
+    glVertex2f(3.7, 3.6);
+    glVertex2f(3.8, 3.63);
+    glVertex2f(4.0, 3.65);
+    glVertex2f(4.2, 3.7);
+    glVertex2f(4.4, 3.7);
+    glVertex2f(4.6, 3.65);
+    glVertex2f(4.8, 3.55);
+    glVertex2f(5.0, 3.45);
+    glVertex2f(5.1, 3.4);
+    glVertex2f(5.2, 3.25);
+    glVertex2f(5.3, 3.2);
+    glVertex2f(5.4, 3.0);
+    glVertex2f(5.5, 2.5);
+
+    glVertex2f(5.45, 2.15);
+    glVertex2f(5.4, 1.9);
+    glVertex2f(5.35, 1.8);
+    glVertex2f(5.2, 1.6);
+    glVertex2f(5.0, 1.5);
+    glVertex2f(4.9, 1.4);
+    glVertex2f(4.7, 1.3);
+    glVertex2f(4.6, 1.27);
+    glVertex2f(4.4, 1.25);
+    glVertex2f(4.0, 1.25);
+    glVertex2f(3.9, 1.3);
+    glVertex2f(3.75, 1.35);
+    glVertex2f(3.6, 1.4);
+    glVertex2f(3.45, 1.55);
+    glVertex2f(3.3, 1.7);
+    glVertex2f(3.2, 1.8);
+    glVertex2f(3.1, 2.2);
+    glEnd();
+    glPopMatrix();
+}
+void Sun()
+{
+    glPushMatrix();
+    float radius = 100;
+    float x = radius;
+    float y = 0;
+    glColor3f(0.9, 0.7, 0.07);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i <= 360; i++)
+    {
+        x = radius * cos(i);
+        y = radius * sin(i);
+        glVertex2f(x + 1700, y + 1800);
+    }
+    glEnd();
+    glPopMatrix();
 }
 void road()
 {
@@ -65,35 +341,6 @@ void road()
     glEnd();
     glPopMatrix();
 }
-void keyboard(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-    case 27:
-        exit(0);
-        break;
-    case 's':
-        pl = pl + 5;
-        display();
-        break;
-    case 'd':
-        a = a - 10;
-        display();
-        break;
-    }
-}
-
-int main(int argc, char **argv)
-{
-    glutInit(&argc, argv);                 // Initialize GLUT
-    glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
-    glutInitWindowSize(320, 320);          // Set the window's initial width & height
-    glutInitWindowPosition(100, 100);      // Position the window's initial top-left corner
-    glutDisplayFunc(display);              // Register display callback handler for window re-paint
-    glutKeyboardFunc(keyboard);
-    glutMainLoop(); // Enter the event-processing loop
-    return 0;
-}
 void sky_scraper()
 {
 
@@ -102,7 +349,7 @@ void sky_scraper()
     // glScaled(0.5,0.5,0.0);
     // outer layer
     glBegin(GL_QUADS);
-    glColor3ub(92,77,69);
+    glColor3ub(92, 77, 69);
     glVertex2f(0, 0);
     glVertex2f(200, 0);
     glVertex2f(200, 700);
@@ -120,7 +367,7 @@ void sky_scraper()
     int inc = 0;
     for (int i = 0; i < 9; i++)
     {
-        glVertex2f(250,40 + inc);
+        glVertex2f(250, 40 + inc);
         glVertex2f(250, 70 + inc);
         glVertex2f(350, 70 + inc);
         glVertex2f(350, 40 + inc);
@@ -128,25 +375,23 @@ void sky_scraper()
     }
     glEnd();
 
-
-    //building 1 glass
+    // building 1 glass
     glBegin(GL_QUADS);
     glColor3ub(193, 215, 215);
     inc = 0;
     for (int i = 0; i < 9; i++)
     {
-        glVertex2f(30,30 + inc);
+        glVertex2f(30, 30 + inc);
         glVertex2f(30, 70 + inc);
         glVertex2f(50, 70 + inc);
         glVertex2f(50, 30 + inc);
 
-
-        glVertex2f(80,30 + inc);
+        glVertex2f(80, 30 + inc);
         glVertex2f(80, 70 + inc);
         glVertex2f(100, 70 + inc);
         glVertex2f(100, 30 + inc);
 
-        glVertex2f(130,30 + inc);
+        glVertex2f(130, 30 + inc);
         glVertex2f(130, 70 + inc);
         glVertex2f(150, 70 + inc);
         glVertex2f(150, 30 + inc);
@@ -506,7 +751,7 @@ void Building()
 void Airplane()
 {
     glPushMatrix();
-    glTranslatef(pl + 100, 1800, 0);
+    glTranslatef(plane_movement + 100, 1800, 0);
     glScaled(5, 5, 0);
     glColor3f(0.0, 0.0, 0.0);
     glBegin(GL_POLYGON); // rectangular body
@@ -573,7 +818,7 @@ void Bus()
 {
     glTranslated(-400, -400, 0);
     glPushMatrix();
-    glTranslated(a, 50.0, 0.0);
+    glTranslated(bus_movement, 50.0, 0.0);
     glScaled(40.0, 40.0, 0.0);
     glColor3f(0.5, 0.0, 0.0);
     // bus out line
@@ -639,7 +884,7 @@ void Bus()
     glPopMatrix();
     // tyre
     glPushMatrix(); // front tyre
-    glTranslated(a + 970, 320, 0.0);
+    glTranslated(bus_movement + 970, 320, 0.0);
     glScaled(20.0, 20.0, 0.0);
     glColor3f(0.9, 0.5, 0.5);
     glBegin(GL_POLYGON);
@@ -686,7 +931,7 @@ void Bus()
     glPopMatrix();
 
     glPushMatrix(); // back tyre
-    glTranslated(a + 1140, 320, 0.0);
+    glTranslated(bus_movement + 1140, 320, 0.0);
     glScaled(20.0, 20.0, 0.0);
     glColor3f(0.9, 0.5, 0.5);
     glBegin(GL_POLYGON);
@@ -731,53 +976,4 @@ void Bus()
     glVertex2f(3.1, 2.2);
     glEnd();
     glPopMatrix();
-}
-void Bird()
-{
-    glPushMatrix();
-    glBegin(GL_POLYGON);
-    glColor3ub(0.0, 0.0, 0.0);
-    float angle41;
-    for (int i = 0; i < 360; i++)
-    {
-        angle41 = i * 3.1416 / 180;
-        glVertex2f(0.8 + 0.04 * cos(angle41), 0.7 + 0.02 * sin(angle41));
-    }
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3ub(0.0, 0.0, 0.0);
-    float angle42;
-    for (int i = 0; i < 360; i++)
-    {
-        angle42 = i * 3.1416 / 180;
-        glVertex2f(0.75 + 0.02 * cos(angle42), 0.71 + 0.02 * sin(angle42));
-    }
-    glEnd();
-
-    glBegin(GL_POLYGON);
-    glColor3ub(255.0, 255.0, 255.0);
-    float angle43;
-    for (int i = 0; i < 360; i++)
-    {
-        angle43 = i * 3.1416 / 180;
-        glVertex2f(0.74 + 0.005 * cos(angle43), 0.71 + 0.005 * sin(angle43));
-    }
-    glEnd();
-    // wing
-
-    glBegin(GL_TRIANGLES);
-    glColor3ub(255.0, 255.0, 255.0);
-    glVertex2f(0.8f, 0.75f);
-    glVertex2f(0.79, 0.71f);
-    glVertex2f(0.785f, 0.8f);
-    glEnd();
-
-    // lip
-    glBegin(GL_TRIANGLES);
-    glColor3ub(255.0, 0.0, 0.0);
-    glVertex2f(0.73f, 0.72f);
-    glVertex2f(0.69f, 0.71f);
-    glVertex2f(0.73f, 0.70f);
-    glEnd();
 }
